bded0e34700f80c7d5edbbb02aea2bee22c9b193
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-26 16:22:19 +0300

    js codes cleaned and some merged in single doument, message posting now working

--- src/reviews.js
+++ src/reviews.js
@@ -1,13 +1,6 @@
 
-//this event listener will activate DOM content loader to load getText function
+//1 . this event listener will activate DOM content loader to load getText function
 document.addEventListener("DOMContentLoader", getText)
-
-//The below code will respond when the user clicks submit button 
-document.getElementById('submit-btn').addEventListener('click', ()=>{
-  window.alert(`Thank you for your feedback We will post your comment after review!`)});
-
-document.getElementById('submit-btn2').addEventListener('click', ()=>{
-  window.alert(`Thank you for signing up for our page. Please stay tuned for email updates!`)});
   
 
 //the code below will be activated once the DOMContentLoaded getText function activared
@@ -30,6 +23,43 @@ function getText(){
 }
 
 getText();
+
+
+//The below addEvent listener codes will respond when the user clicks submit button for posting reviews 
+document.getElementById('submit-btn').addEventListener('click', ()=>{
+  window.alert(`Thank you ${nameElem.value}!`)});
+
+
+//this variables will help access the text input and post them back on top of the comment box
+var messages = document.getElementById("comment");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("submit-btn");
+var nameElem = document.getElementById("nametext");
+
+// this code sill add the h3 element containing the name of the person posting
+button.addEventListener("click", function(){
+    var name = document.createElement("h")
+    name.innerHTML = nameElem.value;
+    name.style.fontWeight = "bold";
+    messages.appendChild(name);
+    nameElem.value =""
+
+// this code sill add the parahgraph containing the message of the person posting
+    var newMessage = document.createElement("p");
+    newMessage.style.listStyle="none";
+    newMessage.style.height = "auto";
+    newMessage.innerHTML = textbox.value;
+    newMessage.style.paddingBottom = "10px";
+    messages.appendChild(newMessage);
+    textbox.value = "";
+});
+
+//3. This codes responds to click events when someone signs up the page.
+document.getElementById('submit-btn2').addEventListener('click', ()=>{
+  window.alert(`Thank you for signing up for our page. Please stay tuned for email updates!`)});
+
+
+
 
 
 
